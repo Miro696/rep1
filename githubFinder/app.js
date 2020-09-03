cd7cb71a0a651025e38d1a3e5786b4fd63e1b5a9
Miro Mro <office@intbool.se>
2020-09-03 13:52:15 +0200

    Added files to project

---
+++ githubFinder/app.js
@@ -0,0 +1,10 @@
+// Search input 
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+ searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+ });
+
+
