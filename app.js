16fe7cc26ff41aec5ecea43801b996fd3e17d567
Miro Mro <office@intbool.se>
2020-09-03 17:01:00 +0200

    Added getElementById and EventListener

---
+++ app.js
@@ -0,0 +1,8 @@
+// Search input 
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+ // Get data input
+  const userText = e.target.value;
+}) 
