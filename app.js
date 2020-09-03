b85bb9b13a5e7e154cd7b3b171c6cd58c02a3e19
Miro Mro <office@intbool.se>
2020-09-03 18:17:46 +0200

    Created class Github in file github.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Init Github class
+const github = new Github;
+
 // Search input 
 const searchUser = document.getElementById('searchUser');
 
@@ -5,4 +8,21 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
  // Get data input
   const userText = e.target.value;
-}) 
+
+  if(userText !== '') {
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+       
+      } else {
+        // Show profile in UI
+
+      }
+
+    }) 
+  } else{
+    // Clear profile from UI
+
+  }
+}); 
---
+++ github.js
@@ -0,0 +1,16 @@
+// Building class 
+class Github {
+  constructor() {
+    this.client_id = 'Iv1.c2fdb5e146f5b9db';
+    this.client_secret = 'bee44a04deb7001c2dc13e46a457785bb5f7d484'
+  }
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+  }
+
+}}
